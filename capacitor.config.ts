264ec411ddcf48d2476e555c31f71206b72767d7
rezlen <soheilyreza@yahoo.com>
2024-05-13 13:57:42 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nuxt.len',
+  appName: 'nuxt-len',
+  webDir: 'dist'
+};
+
+export default config;
