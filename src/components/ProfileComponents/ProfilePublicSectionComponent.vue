410cb962e4d722bae15f6efe9ce358bed88de8f8
rezlen <soheilyreza@yahoo.com>
2024-06-08 23:17:03 +0100

    ProfPubColumns

--- src/components/ProfileComponents/ProfilePublicSectionComponent.vue
+++ src/components/ProfileComponents/ProfilePublicSectionComponent.vue
@@ -1,20 +1,94 @@
 <template>
   <IonGrid class="#">
-    <!-- Profile&StatementRow -->
+    <!-- 5Rows 1- Business Row 2- Personal row 3- Statistcs row  4-WhatWho row 5- Activities row -->
     <IonRow class=" Profile&StatisticsRow BorderedSection">
-      <IonRow class=" BusinessRow BorderedSection"></IonRow>
-      <IonRow class=" PersonalRow BorderedSection"></IonRow>
+      <IonRow class=" BusinessName BorderedSection">
+        <IonTitle>Business Name</IonTitle>
+      </IonRow>
 
-      <IonCol class=" WhoWeAreWhatWeDo BorderedSection"></IonCol>
-      <IonCol class=" WhatWeProvide BorderedSection"></IonCol>
-      <IonCol class=" WhatWeAreLookingFor BorderedSection"></IonCol>
+      <IonRow class=" BusinessRow BorderedSection">
+        <img/>
+        <IonRow class="BusinessRevenue BorderedSection">Business Revenue: £</IonRow>
+        <IonRow>Business Category:</IonRow>
+        <IonCol class=" BorderedSection" >No. of Employees:</IonCol>
+        <IonCol>Business City:</IonCol>
+        <div class="social-icons">
+          <IonIcon :icon="logoGoogle" class="google-icon" />
+          <IonIcon :icon="logoFacebook" class="facebook-icon" />
+          <IonIcon :icon="logoTwitter" class="twitter-icon" />
+          <IonIcon :icon="logoLinkedin" class="linkedin-icon" />
+        </div>
+      </IonRow>
+      <!-- Personal Row ###################-->
+      <IonRow class=" PersonalRow BorderedSection">
+        <IonCol><img/></IonCol>
+        <IonCol class=" BorderedSection" >Name:</IonCol>
+        <IonCol>Age:</IonCol>
+        <IonCol>Gender:</IonCol>
+        <IonCol class=" BorderedSection" >Job Positions:</IonCol>
+        <IonCol>City:</IonCol>
+        <IonCol>Connections:</IonCol>
+        <IonCol class="social-icons">
+          <IonIcon :icon="logoGoogle" class="google-icon" />
+          <IonIcon :icon="logoFacebook" class="facebook-icon" />
+          <IonIcon :icon="logoTwitter" class="twitter-icon" />
+          <IonIcon :icon="logoLinkedin" class="linkedin-icon" />
+        </IonCol>
+        <IonRow class="Yearly Salary">Yearly Salary: £</IonRow>
+      </IonRow>
+
+      <!-- 3- Statistcs row ##############################-->
+      <IonRow class=" Statistcs Row BorderedSection">
+        <IonCol class=" PostedOffers BorderedSection">Posted Offers</IonCol>
+        <IonCol class=" ProvidedOffers BorderedSection">Provided Offers</IonCol>
+        <IonCol class=" RequestedOffers BorderedSection">Requested Offers</IonCol>
+        <IonCol class=" PostedNeeds BorderedSection">Posted Needs</IonCol>
+        <IonCol class=" SatisfiedNeeds BorderedSection">Satisfied Needs</IonCol>
+        <IonCol class=" PeopleSatisfiedNeeds BorderedSection">People Satisfied Needs</IonCol>
+        <IonCol class=" MiniProfileListed BorderedSection">Mini Profile Listed:</IonCol>
+        <IonCol class=" Exhibited  BorderedSection">Exhibited:</IonCol>
+        <IonCol class=" Visited BorderedSection">Visited:</IonCol>
+        <IonCol class=" Messages BorderedSection">Messages</IonCol>
+        <IonCol class=" Connect BorderedSection">Connect</IonCol>
+        <IonCol class=" QRCode BorderedSection">QR code</IonCol>
+      </IonRow>
+
+      <!-- 4- WhoWhat row ##############################-->
+      <IonRow class=" WhoWhatSections BorderedSection" >
+        <IonCol class=" WhoWeAreWhatWeDo BorderedSection">
+            <p>Who We Are, What We Do</p>
+            OUtPUTHERE
+        </IonCol>
+        <IonCol class=" WhatWeProvide BorderedSection">
+          <p>What We Provide</p>
+            OUtPUTHERE
+        </IonCol>
+        <IonCol class=" WhatWeAreLookingFor BorderedSection">
+          <p>What We Are Looking For</p>
+            OUtPUTHERE
+        </IonCol>
+      </IonRow>
+      
+      
+      <!-- 5 - ActivityRow ListedNeedsOffersVisitedEventsConnectionGalery -->
+      <IonRow class=" ActivityRow BorderedSection" >
+        <IonCol class=" ListedNeeds BorderedSection" ></IonCol>
+        <IonCol class=" SatisfiedNeeds BorderedSection" ></IonCol>
+        <IonCol class=" PeopleSatisfiedNeeds BorderedSection" ></IonCol>
+        <IonCol class=" ProvidedOffers BorderedSection" ></IonCol>
+        <IonCol class=" RequestedOffers BorderedSection" ></IonCol>
+        <IonCol class=" PeopleRequestedOffers BorderedSection" ></IonCol>
+        <IonCol class=" ListedBestOffer BorderedSection" ></IonCol>
+        <IonCol class=" VisitedEvents BorderedSection" ></IonCol>
+        <IonCol class=" ExhibitedEvents BorderedSection" ></IonCol>
+        <IonCol class=" Connections BorderedSection" ></IonCol>
+        <IonCol class=" 4Galleries BorderedSection" ></IonCol>
+        <IonCol class=" 3PitchingVideos BorderedSection" ></IonCol>
+      </IonRow>
 
 
     </IonRow>
-    <!-- OffersNeedsEventsRow -->
-    <IonRow class=" OffersNeedsEventsRow BorderedSection">
-    Make Sure the Tab hoverinf is done in the same way as LEN website
-    </IonRow>
+
   </IonGrid>
 </template>
 
