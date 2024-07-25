10f956493ae79be2c3f3077988a25e00c59d46ae
rezlen <soheilyreza@yahoo.com>
2024-07-25 11:14:52 +0100

    commented to load in Netlify

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,10 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
-import { Profile } from './Profile';
+// import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
+// import { Profile } from './Profile';
 
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn() id: number;
-  @Column() email: string;
-  @Column() password: string;
-  @OneToOne(() => Profile, profile => profile.user) profile: Profile;
-}
+// @Entity()
+// export class User {
+//   @PrimaryGeneratedColumn() id: number;
+//   @Column() email: string;
+//   @Column() password: string;
+//   @OneToOne(() => Profile, profile => profile.user) profile: Profile;
+// }
